a5cff829d82b5458caa04121061b09c3c40dba18
baixinxing <baixinxing@caomeijiangx.com>
2018-01-16 21:29:14 +0800

    no message

--- vendor/laravel/framework/src/Illuminate/Config/Repository.php
+++ vendor/laravel/framework/src/Illuminate/Config/Repository.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Config\Repository as ConfigContract;
 class Repository implements ArrayAccess, ConfigContract
 {
     /**
-     * All of the configuration items.
+     * 所有配置元素
      *
      * @var array
      */
